e496933448407a70113ceec7afa709f23eb79ff9
Madmay <mrai748@gmail.com>
2023-12-12 11:13:34 +0530

    Changes

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from setuptools import setup, find_packages
 
+
 # Function to read the contents of the requirements file
 def read_requirements():
-    with open('requirements.txt') as req:
-        return req.read().splitlines()
-
+    with open('requirements.txt', encoding='utf-8') as f:
+        return [line.strip() for line in f if line.strip()]
+    
+    
 setup(
     name='breeze-historical-options',
-    version='1.0',
+    version='1.1',
     author='Mayank Rai',
     author_email='mrai748@gmail.com',
     description='A Python package for easily downloading historical options data',
-    long_description='A Python package for easily downloading second-level historical options data',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/madmay247/breeze-historical-options',
     packages=find_packages(),
     install_requires=read_requirements(),
